667f710b9ccedd12ed530552bd44633d9e04d233
AFFT-520 <conor.rynne62@gmail.com>
2016-06-10 14:13:26 +0100

    fixed bad merge in build script

--- pkg/win32/build.sh
+++ pkg/win32/build.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 cp -rf ../../src ./build
@@ -11,17 +10,3 @@ rm -rf apsw
 cp -rf ./pynsist_pkgs ./build/pynsist_pkgs
 cd build
 python3.5 -m nsist installer.cfg
-=======
-#!/bin/bash
-
-cp -rf ../../src ./build
-cp ../installer.cfg ./build/installer.cfg
-cp ./afft.ico ./build/afft.ico
-mkdir pynsist_pkgs
-7z x ./win-binaries/apsw-3.8.10.1-r1.win32-py3.4.exe -oapsw
-cp -rf apsw ./pynsist_pkgs/apsw
-rm -rf apsw
-cp -rf ./pynsist_pkgs ./build/pynsist_pkgs
-cd build
-python3.5 -m nsist installer.cfg
->>>>>>> origin/Alpha3-Python
